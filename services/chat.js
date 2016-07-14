fa61701257fc309ea0defc716876da58fec26147
Scott Gillatt <scottgillatt@gmail.com>
2016-07-13 20:11:12 -0600

    Toms Changes for Chat.js Imported

--- services/chat.js
+++ services/chat.js
@@ -1,81 +1,83 @@
 (function () {
+	
+	var app = angular.module('ngKanban');
 
-    var app = angular.module('ngKanban');
+	app.factory('chatService', ['$rootScope', '$q', function ($rootScope, $q) {
 
-    app.factory('chatService', ['$rootScope', '$q', function ($rootScope, $q) {
+		subscribeToConversations();
+		
+		function subscribeToConversations(myUserId) {
 
-        function subscribeToConversations(myUserId) {
+			firebase.database().ref('chat/conversations/' + myUserId).on('value', function (snapshot) {
+			
+				var conversations = [];
 
-            // var uid = firebase.auth().currentUser.uid;
+				snapshot.forEach(function (childSnapshot) {
+					conversations.push(childSnapshot.key);
+				});
+		
+				$rootScope.$broadcast('chat-conversations-updated', conversations);
+			});
+		}	
 
-            firebase.database().ref('chat/conversations/' + uid).on('value', function (snapshot) {
+		function subscribeToMessages(conversationId) {
+			
+			firebase.database().ref('chat/messages/' + conversationId).on('value', function (snapshot) {
+			
+				var messages = [];
 
-                var conversations = [];
+				snapshot.forEach(function (childSnapshot) {
+					messages.push(childSnapshot.val());
+				});
+		
+				$rootScope.$broadcast('chat-messages-updated', {
+					conversationId: conversationId,
+					messages: messages
+				});
+			});
+		}
 
-                snapshot.forEach(function (childSnapshot) {
-                    conversations.push(childSnapshot.key);
-                });
+		function getConversationId(myUserId, otherUserId) {
 
-                $rootScope.$broadcast('chat-conversations-updated', conversations);
-            });
-        };
+			var deferred = $q.defer();
+			
+			var myConversations = [];
+			var otherConversations = [];
 
-        function subscribeToMessages(conversationId) {
-            firebase.database().ref('chat/messages/' + conversationId).on('value', function (snapshot) {
+			firebase.database().ref('/chat/conversations/' + myUserId).once('value')
+				.then(
+					function (snapshot) {
+						snapshot.forEach(function (childSnapshot) {
+							myConversations.push(childSnapshot.key);
+						});
 
-                var messages = [];
-
-                snapshot.forEach(function (childSnapshot) {
-                    messages.push(childSnapshot.val());
-                });
-
-                console.log('Conversations: ', conversations)
-
-                $rootScope.$broadcast('chat-messages-updated', {
-                    conversationId: conversationId,
-                    messages: messages
-                });
-            });
-        }
-
-        function getConversationId(otherUserId) {
-
-            var deferred = $q.defer();
-            var myUserId = firebase.auth().currentUser.uid;
-            var myConversations = [];
-            var otherConversations = [];
-
-            firebase.database().ref('/chat/conversations/' + myUserId).once('value')
-                .then(
-                function (snapshot) {
-                    snapshot.forEach(function (childSnapshot) {
-                        myConversations.push(childSnapshot.key);
-                    })
-
-                    return firebase.database().ref('/chat/conversations' + otherUserId).once('value');
-                }
-                )
-                .then(
-                function (snapshot) {
-                    snapshot.forEach(function (childSnapshot) {
-                        otherConversations.push(childSnapshot.key);
-                    });
-
-                    var conversationId = null;
-                    myConversations.forEach(function (item) {
-                        if (otherConversations.indexOf(item) > -1) {
-                            deferred.resolve(item);
-                        };
-                        deferred.reslove(null);
-                    });
-                    return deferred.promise;
-                }
-                );
-
-            return {
-                subscribeToConversations: subscribeToConversations,
-                subscribeToMessages: subscribeToMessages,
-                getConversationId: getConversationId
-            };
-        }]);
+						return firebase.database().ref('/chat/conversations/' + otherUserId).once('value');
+					}
+				)
+				.then(
+					function (snapshot) {
+						snapshot.forEach(function (childSnapshot) {
+							otherConversations.push(childSnapshot.key);
+						});
+						
+						myConversations.forEach(function (item) {
+							if (otherConversations.indexOf(item) > -1) {
+								deferred.resolve(item);
+							}
+						});
+						
+						deferred.resolve(null);
+					}
+				);
+			
+			return deferred.promise;
+		}
+		
+		return {
+			subscribeToConversations: subscribeToConversations,
+			subscribeToMessages: subscribeToMessages,
+			getConversationId: getConversationId
+		};
+	}]);
+	
 })();
